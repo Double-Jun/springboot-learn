cfb0746fe73ed09c26baa7b420bf57fca887a0f8
chenmingjun <chenmingjun199311@126.com>
2017-02-25 23:03:51 +0800

    解决spring容器启动报错

--- src/test/java/com/example/RedisTest.java
+++ src/test/java/com/example/RedisTest.java
@@ -1,8 +1,11 @@
 package com.example;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
 
 /**
  * @Author chenmingjun
@@ -11,5 +14,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RedisTest {
+
+	@Test
+	public void testPing() {
+		Jedis jedis = new Jedis("127.0.0.1", 6379);
+		Assert.assertEquals("PONG", jedis.ping());
+	}
 
 }
